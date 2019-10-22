daecc579b126ffa7ed67668801c3fd2063b64ad4
miruka <miruka@disroot.org>
2019-10-22 03:41:16 -0400

    live-reload.sh: remove outdated comment

--- live-reload.sh
+++ live-reload.sh
@@ -6,8 +6,6 @@
 # An argument can be given to specify which QML file in src/qml to load.
 # If no argument is given, the default is "UI.qml".
 
-# The no_embedded CONFIG option (resources) is used to speed up the compilation
-
 export QT_QPA_PLATFORM=xcb
 
 make clean
