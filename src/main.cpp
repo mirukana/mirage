d53cf0cbbe261f9961a0eb39f262db22b5faaaf9
miruka <miruka@disroot.org>
2020-08-28 10:12:43 -0400

    Bump version to v0.6.2

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]) {
     QApplication::setOrganizationName("mirage");
     QApplication::setApplicationName("mirage");
     QApplication::setApplicationDisplayName("Mirage");
-    QApplication::setApplicationVersion("0.6.1");
+    QApplication::setApplicationVersion("0.6.2");
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
 
