0e3821a5924f437246b74ac332b9c236774a1d04
miruka <miruka@disroot.org>
2020-09-22 21:22:38 -0400

    Prefix event transaction_id keys with reverse DNS
    Respect the Matrix spec by using
    "io.github.mirukana.mirage.transaction_id" instead of
    "mirage.transaction_id".

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -14,4 +14,5 @@ documentation in the following modules first:
 
 __app_name__     = "mirage"
 __display_name__ = "Mirage"
+__reverse_dns__  = "io.github.mirukana.mirage"
 __version__      = "0.6.4"
