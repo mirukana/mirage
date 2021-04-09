mirukana/mirage

commit ba3e93e658eb8c536a0dd803c8cb5fabbb68fb8f

Fix ~/qrc: folder created on start in release mode

src/backend/user_files.py
@@ -168,7 +168,8 @@ class UserFile:
     async def _start_writer(self) -> None:
         """Disk writer coroutine, update the file with a 1 second cooldown."""
 
-        self.write_path.parent.mkdir(parents=True, exist_ok=True)
+        if self.write_path.parts[0] == "qrc:":
+            return
 
         while True:
             await asyncio.sleep(1)

commit 6ad8d54e19410106b5abe764b84dde9bc3a19921

Fix config writing failing if parent dir not exist
Problem introduced in ba3e93e658eb8c536a0dd803c8cb5fabbb68fb8f

src/backend/user_files.py
@@ -171,6 +171,8 @@ class UserFile:
         if self.write_path.parts[0] == "qrc:":
             return
 
+        self.write_path.parent.mkdir(parents=True, exist_ok=True)
+
         while True:
             await asyncio.sleep(1)
 