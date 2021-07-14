64454e64dfde0c7155b834301a75ccc4a43dcaf8
miruka <miruka@disroot.org>
2021-07-13 23:15:12 -0400

    Remove some unused imports

--- src/backend/nio_callbacks.py
+++ src/backend/nio_callbacks.py
@@ -1,7 +1,6 @@
 # Copyright Mirage authors & contributors <https://github.com/mirukana/mirage>
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-import asyncio
 import json
 import logging as log
 from dataclasses import dataclass, field
